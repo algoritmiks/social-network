cbbb3af151255b6490bc5109eb3e2e7b95a5e6dd
Aleksey Osipov <alexey11rus@gmail.com>
2019-11-12 16:07:27 +0300

    dialogs page added

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -3,8 +3,36 @@ import css from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={css.dialogs}>
+            <div className={css.dialogsItems}>
+                <div className={`${css.dialog} ${css.active}`}>
+                    Sveta
+                </div>
+
+                <div className={css.dialog}>
+                    Matt
+                </div>
+
+                <div className={css.dialog}>
+                    Peter
+                </div>
+
+                <div className={css.dialog}>
+                    Jon
+                </div>
+
+            </div>
+            <div className={css.messages}>
+                <div className={css.msg}>
+                    Hello!
+                </div>
+                <div className={css.msg}>
+                    How are you?
+                </div>
+                <div className={css.msg}>
+                    I'm OK! And how are you?
+                </div>
+            </div>
         </div>
     );
 }
