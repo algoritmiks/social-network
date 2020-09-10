ee2ceb8c612ba0e1cdbc6b9d80e75387c619cf3c
Aleksey Osipov <alexey11rus@gmail.com>
2020-09-10 13:50:15 +0300

    refactor: preloader change

--- src/components/common/preloader/preloader.js
+++ src/components/common/preloader/preloader.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import preloader from './preloader.svg'  //Shouldn't use this import with alternate preloader
 
 const Preloader = (props) => {
 
-    return <><img src={'/img/preloader.svg'} alt="preloader"></img></>
+    return (
+      <div style={{backgroundColor: 'white'}}>  
+        <img src={preloader} alt="preloader"/>   
+      </div>
 
+    // return <><img src={'/img/preloader.svg'} alt="preloader"></img></>  //Alternate use preloader
+    )
 }
 
 export default Preloader;
