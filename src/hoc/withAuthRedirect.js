af5e3f43dbc740db2dfe2295a66f541478753c5f
Aleksey Osipov <alexey11rus@gmail.com>
2020-11-07 20:18:28 +0300

    refactor: comments delete

--- src/hoc/withAuthRedirect.js
+++ src/hoc/withAuthRedirect.js
@@ -15,21 +15,3 @@ export const withAuthRedirect = (Component) => {
   const IsAuthConnected = connect(isAuthStateToProps)(RedirectComponent);
   return IsAuthConnected;
 }
-
-
-
-// export const withAuthRedirect = (Component) => {
-
-//   class RedirectComponent extends React.Component {
-//       render() {
-//           if (!this.props.isAuth) return <Redirect to='/login' />
-
-//           return <Component {...this.props}/>
-//       }
-//   }
-
-//   let ConnectedAuthRedirectComponent = connect(mapStateToPropsForRedirect)(RedirectComponent)
-
-//   return ConnectedAuthRedirectComponent;
-
-// }
