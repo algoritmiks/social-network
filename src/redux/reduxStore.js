4ada24a0d68e415e82bc6debcfc17fc3c15b5dc5
Aleksey Osipov <alexey11rus@gmail.com>
2021-02-19 16:32:27 +0300

    feat: add chrome redux-dev-tools extention

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import profileReducer from './profileReducer';
 import dialogReducer from './dialogsReducer';
 import usersReducer from './usersReducer';
@@ -18,7 +18,10 @@ let reducers = combineReducers({
     form: formReducer
 });
 
-let store = createStore(reducers, applyMiddleware(thunk));
-window.store = store;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers,  composeEnhancers(applyMiddleware(thunk)));
+
+// let store = createStore(reducers, applyMiddleware(thunk));
+window._store = store;
 
 export default store;
